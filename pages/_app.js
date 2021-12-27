1d19e8ed2ddcf886b192e255d355b6d4bf0ea3c1
Muhammad Hassan <hassanmuhammad436@gmail.com>
2021-12-28 00:22:46 +0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -84,7 +84,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>NextJS Material Dashboard by Creative Tim</title>
+          <title>MS Thesis Management System</title>
           <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
         </Head>
         <Layout>
